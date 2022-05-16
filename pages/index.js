eadef210f9f3be8fe8536c3443b86a6f646ce143
Avdyeyenko Vitalik <dalshe.budet.sobaka@gmail.com>
2022-05-16 17:38:45 +0300

    Dybamic post pages implemented

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,20 @@ import Layout from '../components/layout';
 import Head from 'next/head';
 import Link from 'next/link';
 import { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
+import Date from '../components/date';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -19,6 +31,29 @@ export default function Home() {
             <a>here!</a>
           </Link>
         </p>
+      </section>
+
+      <section>
+        <ul className={utilStyles.list}>
+          {
+            allPostsData.map(({ id, date, title }) => (
+              <li 
+                key={id}
+                className={utilStyles.listItem}
+              >
+                <Link href={`/posts/${id}`}>
+                  <a>{title}</a>
+                </Link>
+
+                <br />
+                
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
+              </li>
+            ))
+          }
+        </ul>
       </section>
     </Layout>
   );
